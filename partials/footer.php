04c223a4ac60695d579c74f5d1a2f7b1aa2dced9
ngkhdy <dynguyenak@gmail.com>
2024-07-23 07:58:46 +0700

    Duy can het

---
+++ partials/footer.php
@@ -0,0 +1,62 @@
+ <!-- cuoi -->
+ <div class="footer">
+ 	<div class="grid_full-width">
+ 		<div class="grid">
+ 			<div class="row">
+ 				<div class="col col-4 col-mobi">
+ 					<div class="logo logo-bottom ml-mobi">
+ 						<img width="40px" src="./public/img/logo1-2.png" alt="" class="img">
+ 					</div>
+ 					<div class="footer__text ml-mobi">
+ 						<p>Khánh Duy Mobile thành lập năm 2025. Chúng
+ 							tôi chuyên bán điện thoại xách tay cao cấp.</p>
+ 					</div>
+ 				</div>
+ 				<div class="col col-4 col-mobi">
+ 					<div class="footer__about">
+ 						<h3>Địa chỉ</h3>
+ 					</div>
+ 					<div class="footer__text">
+ 						<a href="https://maps.app.goo.gl/166vevaGWxz9Fuze9">193 Đỗ Văn Thi, Phường, Thành phố Biên Hòa, Đồng Nai
+ 							700000, Việt Nam</a>
+ 						<iframe width="100%" height="50%"
+ 							src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11343.901336392995!2d106.83472667155728!3d10.945154558059988!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3174dffcc5ecddb9%3A0xc8971ea2ed8e08a1!2zVHLGsOG7nW5nIENhbyDEkeG6s25nIEZQVCBQb2x5dGVjaG5pYyBjxqEgc-G7nyDEkOG7k25nIE5haQ!5e0!3m2!1svi!2s!4v1721435596958!5m2!1svi!2s"
+ 							width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+ 							referrerpolicy="no-referrer-when-downgrade"></iframe>
+ 					</div>
+ 				</div>
+ 				<div class="col col-4 col-mobi">
+ 					<div class="footer__about">
+ 						<h3>Dịch vụ</h3>
+ 					</div>
+ 					<div class="footer__text">
+ 						<p>
+ 							Bảo hành rơi vỡ, ngấm nước Care Diamond
+ 						</p>
+ 						<p>
+ 							Bảo hành Care X60 rơi vỡ ngấm nước vẫn Đổi
+ 							mới
+ 						</p>
+ 					</div>
+ 				</div>
+ 				<div class="col col-4 col-mobi">
+ 					<div class="footer__about">
+ 						<h3>Liên hệ</h3>
+ 					</div>
+ 					<div class="footer__text">
+ 						<p>Phone Sale: <a href="tel:+84 399902410">(+84) 039 990 2410</a></p>
+ 						<p>Email: <a href="mailto:dynguyenak@gmail.com">dynguyenak@gmail.com</a></p>
+ 					</div>
+ 				</div>
+ 			</div>
+ 		</div>
+ 	</div>
+ </div>
+ <div class="header">
+ 	<div class="header-container">
+ 		<header class="header-bottom">
+ 			<span>2018 ©
+ 			</span>
+ 		</header>
+ 	</div>
+ </div>
---
+++ user.php
@@ -0,0 +1,106 @@
+<?php
+session_start();
+$id = $_SESSION['id'];
+if (empty($_SESSION['id'])) {
+	$_SESSION['error'] = 'Đăng nhập đi bạn!';
+	header('location:login.php');
+}
+
+require './admin/root.php';
+$sql = "SELECT * FROM `customers` WHERE `id`= {$id} ";
+$result = mysqli_query($connect, $sql);
+if (empty($result)) {
+	header('location:../partials/404.php');
+}
+
+include './partials/sticky.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="./public/css/rss.css" />
+	<link rel="stylesheet" href="./public/css/style.css" />
+	<link rel="stylesheet" href="./public/css/pages.css" />
+
+	<title>Thông tin người dùng</title>
+	<style>
+		a {
+			color: red;
+			text-decoration: none;
+		}
+	</style>
+</head>
+
+<body>
+	Day la trang nguoi dung. Xin chao ban
+	<?php
+	echo $_SESSION['name'];
+
+	?>
+	<br>
+	<div class="row mt-3">
+		<div class="col">
+			<div class="table-responsive">
+				<table class="table table-bordered mt-1 align-middle" id="dataTable" width="100%" cellspacing="0">
+					<thead class="thead-dark">
+						<tr>
+							<th>Id</th>
+							<th>Fullname</th>
+							<th>Email</th>
+							<th>Phone</th>
+							<th>Gender</th>
+							<th>Address</th>
+							<th>Status</th>
+							<th>Created</th>
+
+						</tr>
+					</thead>
+					<tbody class="thead-light">
+						<?php foreach ($result as $each) : ?>
+							<tr class="text-dark">
+								<td class="text-primary"> <?php echo $each['id']; ?></td>
+								<td class="text-primary"> <?php echo $each['name']; ?></td>
+								<td class="text-primary"> <?php echo $each['email']; ?></td>
+								<td class="text-primary"> <?php echo $each['phone']; ?></td>
+								<td class="text-primary">
+									<?php if ($each['gender'] == 1) { ?>
+										Nam
+									<?php } else { ?>
+										Nữ
+									<?php } ?>
+								</td>
+								<td class="text-primary"> <?php echo $each['address']; ?></td>
+								<td class="text-success">
+									<?php if ($each['status'] == 1) { ?>
+										Active
+									<?php } else { ?>
+										<span class="text-danger">
+											Not Active
+										</span>
+
+									<?php } ?>
+								</td>
+								<td class="text-primary"> <?php echo $each['created_at']; ?></td>
+
+
+							</tr>
+						<?php endforeach ?>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</div>
+
+
+
+
+	<a href="logout.php">
+		Dang xuat
+	</a>
+</body>
+
+</html>
