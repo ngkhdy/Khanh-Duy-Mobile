b9070bc87766ea5b3f5f42825de0bf23eaf80fed
HP_Nam <namphg1412@gmail.com>
2024-07-27 07:57:59 +0700

    change mail config

--- sendmail/server/send-mail.php
+++ sendmail/server/send-mail.php
@@ -10,32 +10,33 @@ require 'src/SMTP.php';
 
 function mySendMail($email, $title, $name, $content)
 {
-	$mail = new PHPMailer(true);
-	try {
-		//Server settings
-		// $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail = new PHPMailer(true);
+    try {
+        //Server settings
+        // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
 
-		$mail->isSMTP();                                           //Send using SMTP
-		$mail->Host       = 'smtp.gmail.com';                  //Set the SMTP server to send through
-		$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-		$mail->Username   = 'duynkpi00114@gmail.com';
-		$mail->Password   = 'nguyenkhanhduy';                           //SMTP password
-		$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;         //Enable implicit TLS encryption
-		$mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-		$mail->SMTPSecure = "tls";
-		$mail->CharSet = "UTF-8";
+        $mail->isSMTP();                                           //Send using SMTP
+        $mail->Host       = 'smtp.gmail.com';                  //Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+        $mail->Username   = 'namhg911@gmail.com';
+        $mail->Password   = 'psuz wviu vown gkay';                           //SMTP password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;         //Enable implicit TLS encryption
+        $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+        $mail->SMTPSecure = "tls";
+        $mail->CharSet = "UTF-8";
 
-		//Recipients
-		$mail->setFrom('duynkpi00114@gmail.com', 'Khanh Duy Mobile');
-		$mail->addAddress($email, $name); // gui den ai
-		//Attachments
-		//Content
-		$mail->isHTML(true);                                  //Set email format to HTML
-		$mail->Subject = $title;
-		$mail->Body    = $content;
-		// $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-		$mail->send();
-	} catch (Exception $e) {
-		echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-	}
+        //Recipients
+        $mail->setFrom('namhg911@gmail.com', 'ShopAdu');
+        $mail->addAddress($email, $name); // gui den ai 
+        //Attachments
+        //Content
+        $mail->isHTML(true);                                  //Set email format to HTML
+        $mail->Subject = $title;
+        $mail->Body    = $content;
+        // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+        $mail->send();
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
 }
+
