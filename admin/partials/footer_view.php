71d7a27878861a31ecb1e978fd01ebe7d84eb297
HP_Nam <namphg1412@gmail.com>
2024-07-31 10:39:25 +0700

    Copyright 2024 of administrator page footer

--- admin/partials/footer_view.php
+++ admin/partials/footer_view.php
@@ -5,7 +5,7 @@
             <footer class="sticky-footer bg-white">
                 <div class="container my-auto">
                     <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; Your Website 2021</span>
+                        <span>Copyright &copy; Mô Bai Sốp 2024</span>
                     </div>
                 </div>
             </footer>
